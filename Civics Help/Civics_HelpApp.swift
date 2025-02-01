b005de2dc7dd4a9fa6263f2c72114c8c1b436946
Mika-Novak <kamillanovak30@gmail.com>
2025-02-01 11:40:39 -0500

    Initial Commit

---
+++ Civics Help/Civics_HelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Civics_HelpApp.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Civics_HelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Civics HelpTests/Civics_HelpTests.swift
@@ -0,0 +1,17 @@
+//
+//  Civics_HelpTests.swift
+//  Civics HelpTests
+//
+//  Created by Kamilla Urunbaeva on 2/1/25.
+//
+
+import Testing
+@testable import Civics_Help
+
+struct Civics_HelpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
