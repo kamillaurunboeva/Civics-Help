c5539f7b3a24c1ea90347435df85ea41faf44819
Kamilla Urunboeva <152669031+kamillaurunboeva@users.noreply.github.com>
2025-02-12 20:04:36 -0500

    Add files via upload

---
+++ ZipCodeEntryView.swift
@@ -0,0 +1,56 @@
+//
+//  ZipCodeEntryView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/11/25.
+//
+
+import SwiftUI
+
+struct ZipCodeEntryView: View {
+    @State private var zipCode: String = ""
+    @State private var showRepresentativeView = false
+    
+    var body: some View {
+        VStack {
+            Text("Enter Your Zip Code")
+                .font(.title)
+                .padding()
+            
+            TextField("Zip Code", text: $zipCode)
+                .keyboardType(.numberPad)
+                .padding()
+                .frame(height: 50)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .padding()
+            
+            Button(action: {
+                if zipCode.count == 5 && Int(zipCode) != nil {
+                    showRepresentativeView = true
+                } else {
+                    // Show an alert or error message
+                    print("Invalid zip code")
+                }
+            }) {
+                Text("Find My Representatives")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.cyan)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+        }
+        .padding()
+        .navigationDestination(isPresented: $showRepresentativeView) {
+                RepresentativeListView(zipCode: zipCode)
+        }
+    }
+}
+
+#Preview {
+    ZipCodeEntryView()
+}
