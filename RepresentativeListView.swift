88c22ddb2d2a865f7c6ab18cda9f6985c570b299
Kamilla Urunboeva <152669031+kamillaurunboeva@users.noreply.github.com>
2025-02-13 23:17:38 -0500

    Add files via upload

---
+++ RepresentativeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RepresentativeListView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/12/25.
+//
+
+import SwiftUI
+
+struct RepresentativeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepresentativeListView()
+}
