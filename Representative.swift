a792d4eaefabef5e101cb58b7359a1092d2baeb3
Kamilla Urunboeva <152669031+kamillaurunboeva@users.noreply.github.com>
2025-02-19 02:27:00 -0500

    Add files via upload

---
+++ Representative.swift
@@ -0,0 +1,15 @@
+//
+//  Representative.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/16/25.
+//
+
+import Foundation
+
+struct Representative: Identifiable {
+    let id = UUID()
+    let name: String
+    let position: String
+    let party: String
+}
---
+++ StarredTestView.swift
@@ -0,0 +1,18 @@
+//
+//  StarredTestView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/1/25.
+//
+
+import SwiftUI
+
+struct StarredTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarredTestView()
+}
