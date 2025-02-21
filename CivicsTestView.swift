927679f997c9aca97c45e99c27d1ae86e1fc875c
Kamilla Urunboeva <152669031+kamillaurunboeva@users.noreply.github.com>
2025-02-20 23:40:51 -0500

    Add files via upload

---
+++ CivicsTestView.swift
@@ -0,0 +1,18 @@
+//
+//  CivicsTestView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 2/17/25.
+//
+
+import SwiftUI
+
+struct CivicsTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CivicsTestView()
+}
