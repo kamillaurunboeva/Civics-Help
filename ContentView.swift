53844740a2daf850aaec07c6d29376b248c3119c
Kamilla Urunboeva <152669031+kamillaurunboeva@users.noreply.github.com>
2025-02-23 17:50:17 -0500

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -12,43 +12,50 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-    
-                VStack {
-                    Text("Civics Help")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .frame(maxHeight: .infinity, alignment: .top)
-                        .padding(.top, 0)
+         
+                ZStack{
+                    Color(.systemGray6)
+                    .ignoresSafeArea()
                     
-                    
-                    LazyVGrid(columns: columns, spacing: 15) {
-                        ForEach(QuizCategory.allCases, id: \.self) { category in
-                            NavigationLink(destination: DetailView(category: category)) {
-                                CategoryCard(category: category)
+                    VStack {
+                        Text("Civics Help")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .frame(maxHeight: .infinity, alignment: .top)
+                            .padding(.top, 0)
+                        
+                        
+                        LazyVGrid(columns: columns, spacing: 15) {
+                            ForEach(QuizCategory.allCases, id: \.self) { category in
+                                NavigationLink(destination: DetailView(category: category)) {
+                                    CategoryCard(category: category)
+                                }
                             }
                         }
+                        .padding()
+                        
+                        Spacer()
+                        
+                        //                    Button(action: {
+                        //                        print("Next button tapped")
+                        //                    }) {
+                        //                        Text("NEXT")
+                        //                            .font(.headline)
+                        //                            .frame(maxWidth: .infinity)
+                        //                            .padding()
+                        //                            .background(Color.red)
+                        //                            .foregroundColor(.white)
+                        //                            .cornerRadius(10)
+                        //                    }
+                        
+                            .padding()
+                        
+                        
                     }
                     .padding()
-                    
-                    Spacer()
-                    
-//                    Button(action: {
-//                        print("Next button tapped")
-//                    }) {
-//                        Text("NEXT")
-//                            .font(.headline)
-//                            .frame(maxWidth: .infinity)
-//                            .padding()
-//                            .background(Color.red)
-//                            .foregroundColor(.white)
-//                            .cornerRadius(10)
-//                    }
-                    
-                    .padding()
-                    
-                }
-                .padding()
-                .frame(maxHeight: .infinity, alignment: .top)
+                    .frame(maxHeight: .infinity, alignment: .top)
+            }
+                
         
         }
 
