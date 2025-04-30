6e2dbfa89c78c550bcf73e48f3bc15ce1532830f
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-04-30 00:16:31 -0400

    Update Question.swift
    updated

--- Question.swift
+++ Question.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Question: Codable, Identifiable {
+struct Question: Codable, Identifiable, Hashable {
     var id = UUID()
     let answer: Int
     let question: LocalizedText
@@ -16,14 +16,27 @@ struct Question: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case answer, question, answers
     }
+
+    init(answer: Int, question: LocalizedText, answers: [Answer]) {
+        self.answer = answer
+        self.question = question
+        self.answers = answers
+    }
+
+    static func == (lhs: Question, rhs: Question) -> Bool {
+        return lhs.id == rhs.id
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
 }
 
-struct Answer: Codable {
+struct Answer: Codable, Hashable {
     let num: Int
     let text: LocalizedText
 }
 
-struct LocalizedText: Codable {
+struct LocalizedText: Codable, Hashable {
     let eng: String
     let rus: String
 }
