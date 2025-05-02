efeb708d4fa1cb6f165e23c4e83bd9a1277931d6
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-01 22:45:10 -0400

    Update StarredQuestions.swift

--- StarredQuestions.swift
+++ StarredQuestions.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class StarredQuestions: ObservableObject {
+    @Published var questions: [Question] = []
+    
+    func load(from allQuestions: [Question]) {
+        if let savedIds = UserDefaults.standard.array(forKey: "StarredQuestions") as? [String] {
+            let savedUUIDs = savedIds.compactMap { UUID(uuidString: $0) }
+            self.questions = allQuestions.filter { savedUUIDs.contains($0.id) }
+        }
+     }
+}
