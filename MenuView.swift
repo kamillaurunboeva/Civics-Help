797e60186157aa2c789888bddfe3192ac0a04696
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-12 20:12:26 -0400

    Add files via upload

---
+++ MenuView.swift
@@ -0,0 +1,62 @@
+//
+//  MenuView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 5/12/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+
+        var body: some View {
+            NavigationStack {
+                ZStack {
+                    Color(.systemGray6)
+                        .ignoresSafeArea()
+
+                    VStack {
+                        Text("Civics Help")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .padding(.top, 40)
+
+                        LazyVGrid(columns: columns, spacing: 15) {
+                            ForEach(QuizCategory.allCases, id: \.self) { category in
+                                NavigationLink(destination: destinationView(for: category)) {
+                                    CategoryCard(category: category)
+                                }
+                            }
+                        }
+                        .padding()
+
+                        Spacer()
+                    }
+                    .padding()
+                    .frame(maxHeight: .infinity, alignment: .top)
+                }
+            }
+        }
+
+        @ViewBuilder
+        func destinationView(for category: QuizCategory) -> some View {
+            switch category {
+            case .civicsTest:
+                CivicsTestView()
+            case .starredTest:
+                StarredTestView()
+            case .stateRepresentative:
+                ZipCodeEntryView()
+            case .studyMaterial:
+                StudyMaterialView()
+            default:
+                DetailView(category: category)
+            }
+        }
+    }
+
+#Preview {
+    MenuView()
+}
