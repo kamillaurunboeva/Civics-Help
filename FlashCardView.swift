efe9f8d47754cc53c9cf9927b1604a0768da026b
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-14 22:48:53 -0400

    Add files via upload

---
+++ FlashCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FlashCardView.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 5/14/25.
+//
+
+import SwiftUI
+
+struct FlashCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashCardView()
+}
