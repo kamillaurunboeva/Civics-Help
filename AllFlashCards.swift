40b0e426cefdffcd7f0e3a8c19c20ef7e406613e
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-17 22:38:31 -0400

    Add files via upload

---
+++ AllFlashCards.swift
@@ -0,0 +1,18 @@
+//
+//  AllFlashCards.swift
+//  Civics Help
+//
+//  Created by Kamilla Urunbaeva on 5/17/25.
+//
+
+import SwiftUI
+
+struct AllFlashCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllFlashCards()
+}
